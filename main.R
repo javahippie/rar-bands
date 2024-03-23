54900533f6bd9adec94a02c07c5cd541f8c15544
Tim Zöller <tim.zoeller@lambdaschmiede.com>
2024-03-23 22:14:20 +0100

    removed German words from excel

--- main.R
+++ main.R
@@ -7,9 +7,9 @@ con <- dbConnect(duckdb::duckdb(), ":memory:")
 dbExecute(con, "INSTALL spatial")
 dbExecute(con, "LOAD spatial")
 dbExecute(con, "CREATE TABLE bands AS SELECT * FROM ST_Read('RaR Bands.xlsx')")
-bands_male <- dbGetQuery(con, "SELECT * FROM bands where main_person_gender = 'männlich'")
+bands_male <- dbGetQuery(con, "SELECT * FROM bands where main_person_gender = 'male'")
 
-bands_non_male <- dbGetQuery(con, "SELECT * FROM bands where main_person_gender <> 'männlich'")
+bands_non_male <- dbGetQuery(con, "SELECT * FROM bands where main_person_gender <> 'male'")
 
 ggplot(bands_male, aes(x=main_person_age))  + 
   geom_histogram(
